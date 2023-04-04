a018280f694966603ce565a71761529e80b5fce6
jryszka <jason@cleverinvestor.com>
2023-04-04 08:41:17 -0700

    stream-pi scripts added

---
+++ stream-pi/env-learn.clever.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env fish
+xdg-open /home/jason/Development/Code\ Workspaces/wp-content.code-workspace
---
+++ stream-pi/env-test.learn.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env fish
+xdg-open /home/jason/Development/Code\ Workspaces/test.learn.clever.code-workspace
