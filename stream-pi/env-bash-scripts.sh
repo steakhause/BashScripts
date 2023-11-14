03ea12bf5b17d6035a9269d90933df5c272420af
steakhause <jason.ryszka@gmail.com>
2023-11-14 15:57:37 -0700

    added more dev environments

---
+++ stream-pi/env-bash-scripts.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/bash
+code /home/jason/Development/Code\ Workspaces/BashScripts.code-workspace;
+terminator -l bash-scripts >/dev/null 2>&1;
+xdotool super+2;
---
+++ stream-pi/env-dodealswithme.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/bash
+code /home/jason/Development/Code\ Workspaces/dodealswithme.code-workspace;
+terminator -l dodealswithme >/dev/null 2>&1;
+xdotool super+2;
