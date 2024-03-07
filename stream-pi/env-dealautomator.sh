ea67d79f801a63c88ae1f35ac8f474fe4a833454
steakhause <jason.ryszka@gmail.com>
2024-03-07 12:13:11 -0700

    new scripts

---
+++ stream-pi/env-dealautomator.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/bash
+code /home/jason/Development/Code\ Workspaces/deal-automator.code-workspace;
+terminator -l dealautomator >/dev/null 2>&1;
+xdotool super+2;
---
+++ stream-pi/toggle-den-fan.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/bash
+curl -X POST http://192.168.0.97:8123/api/webhook/-ToggleDenFanOnOrOff
