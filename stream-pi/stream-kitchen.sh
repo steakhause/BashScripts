3e8857e4f8f73d48545cfec1fe56e91e559f6751
steakhause <jason.ryszka@gmail.com>
2024-04-25 11:55:31 -0700

    updated ip addresses

--- stream-pi/stream-kitchen.sh
+++ stream-pi/stream-kitchen.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env fish
-ffplay -top 535 -left 1400 -volume 0 -x 640 -y 360 -alwaysontop 'rtsp://admin:OfftheRoof$@192.168.0.7:554/cam/realmonitor?channel=1&subtype=0' &
+ffplay -top 535 -left 1400 -volume 0 -x 640 -y 360 -alwaysontop 'rtsp://admin:OfftheRoof$@10.0.30.6:554/cam/realmonitor?channel=1&subtype=0' &
